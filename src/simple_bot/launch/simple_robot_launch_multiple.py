95e92f441021318475a276ea9e315dc20e35ef63
chris <qibitech.com>
2024-04-03 16:59:22 +0900

    lidar driver dict

--- src/simple_bot/launch/simple_robot_launch_multiple.py
+++ src/simple_bot/launch/simple_robot_launch_multiple.py
@@ -10,27 +10,33 @@ import os
 # The user selects which lidar driver to use by assigning a value to the 'lidar' launch argument
 argument = DeclareLaunchArgument('lidar', default_value='sick551', 
                                  choices=['sick551', 'sick571', 'ust10lx', 'ust20lx', 'pacecat', 'slamtech'])
-# Each lidar driver has a corresponding boolean value showing if it is used or not
-lidar_drivers = {'sick551':False, 'sick571':False, 'ust10lx':False,
-                  'ust20lx':False, 'pacecat': False, 'slamtech': False}
+# # Each lidar driver has a corresponding boolean value showing if it is used or not
+# lidar_drivers = {'sick551':False, 'sick571':False, 'ust10lx':False,
+#                   'ust20lx':False, 'pacecat': False, 'slamtech': False}
+## TODO
 lidar_drivers = {
-    'SLAMTEC_LPX_T1': {
-        'launch_file': 'view_sllidar_t1_launch.py',
-        'args': []
-    },
-    'PaceCat_2D_LDS_50C_E': {
-        'launch_file': 'LDS-50C-E.launch',
-        'args': []
-    },
-    'SICK_TIM571': {
-        'launch_file': 'sick_tim_5xx.launch.py',
-        'args': [('hostname', '192.168.0.10')]
-    }
+    'sick551':[os.path.join(get_package_share_directory('sick_scan_xd'), 
+                                                    'launch'),'sick_tim_5xx.launch.py'],
+    'sick571':[os.path.join(get_package_share_directory('sick_scan_xd'), 
+                                                    'launch'),'sick_tim_5xx.launch.py'], 
+    'ust10lx':[os.path.join(get_package_share_directory('urg_node2'), 
+                                                    'launch'),'urg_node2.launch.py'],
+    'ust20lx':[os.path.join(get_package_share_directory('urg_node2'), 
+                                                    'launch'),'urg_node2.launch.py'],
+    'pacecat':[os.path.join(get_package_share_directory('bluesea2'), 
+                                                    'launch'),'LDS-50C-E.launch'],
+    'slamtech':[os.path.join(get_package_share_directory('sllidar_ros2'), 
+                                                    'launch'),'sllidar_t1_launch.py'],
 }
+
+## END TODO
 def generate_launch_description():
     # Switch the boolean corresponding to the required lidar driver to True
     lidar = LaunchConfiguration('lidar')
-    lidar_drivers[lidar] = True
+    try:
+        lidar_driver = lidar_drivers[lidar]
+    except KeyError:
+        rospy.logerr(f"Lidar driver for {lidar} not found")
 
     # Get the path of the URDF file and the rviz configuration
     pkg_share = launch_ros.substitutions.FindPackageShare(package='simple_bot').find('simple_bot')
@@ -39,6 +45,7 @@ def generate_launch_description():
     # TODO: select the correct rviz config
     default_rviz_config_path = os.path.join(pkg_share, 'config/slam_toolbox_default.rviz')
 
+    # FIXME
     with open(default_model_path, 'r') as urdf:
         robot_desc = urdf.read()
 
@@ -64,18 +71,14 @@ def generate_launch_description():
 
     
     #TODO: add all drivers
-    # ros2 launch bluesea2 LDS-50C-E.launch
-    # ros2 launch sllidar_ros2 view_sllidar_t1_launch.py
-    sick571_driver = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('sick_scan_xd'), 
-                                                    'launch'),'sick_tim_5xx.launch.py']),
-        launch_arguments={'hostname':'192.168.0.10'}.items(),
-        condition=IfCondition(lidar_drivers['sick571'])
+    driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(lidar_driver),
+        launch_arguments={'hostname':'192.168.0.10'}.items(),  # FIXME doesn't work
     )
 
     return launch.LaunchDescription([
         argument,
-        sick571_driver,
+        driver_launch,
         joint_state_publisher_node,
         robot_state_publisher_node,
         rviz_node
